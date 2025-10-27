01c6b21a417b11bf55e0606d98b5fc41e1111a21
maskikuy <kproject305@gmail.com>
2025-10-27 09:21:23 +0700

    memperbaiki true menjadi false

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 const db = require('./models');
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ 
+    extended: false 
+}));
 
 app.listen(port, () => {
     console.log(`server started on port 3000`);
